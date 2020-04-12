8cb169e340dec88eda6b660fd28307ffb5f12519
soyoil <kappamaki8801@gmail.com>
2020-04-12 21:47:53 +0900

    Initial Commit

---
+++ YU-GI-OH-Calc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YU-GI-OH-Calc
+//
+//  Created by Akifumi Soyama on 2020/04/12.
+//  Copyright © 2020 soyoil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
